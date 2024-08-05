0d0cda6602d3f960a8024a325bbf75497f5bf2b9
ioancallay <ivanar71@uniandes.edu.ec>
2024-08-05 12:10:18 -0500

    #update
    Update login form

---
+++ resources/views/tester.blade.php
@@ -0,0 +1,11 @@
+<x-master-layout>
+    <div class="header">
+        <h1 class="header-title">
+            WebTech APP
+        </h1>
+        <p class="header-subtitle">{{ __('Dashboard') }}</p>
+    </div>
+    <div class="row">
+
+    </div>
+</x-master-layout>
