ab5997fdb6e4a95b4cb523d5b3d2dea332f95a3d
ioancallay <ivanar71@uniandes.edu.ec>
2024-08-06 13:49:03 -0500

    iancallay
    Cambios en la presentación de los formularios

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -9,15 +9,15 @@
             <div class="card">
                 <div class="card-header"></div>
                 <div class="card-body">
-                    <h1>Crear Permiso</h1>
+                    <h1>{{ __('Create permission') }}</h1>
                     <form action="{{ route('permissions.store') }}" method="POST">
                         @csrf
                         <div class="mb-3">
-                            <label for="name" class="form-label">{{ __('Name') }}</label>
+                            <label for="name" class="form-label">{{ __('Permission name') }}</label>
                             <input type="text" class="form-control" id="name" name="name" required>
                         </div>
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                        <a href="{{ route('permissions.index') }}" class="btn btn-secondary ms-2">Cancelar</a>
+                        <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                        <a href="{{ route('permissions.index') }}" class="btn btn-secondary ms-2">{{ __('Cancel') }}</a>
                     </form>
                 </div>
             </div>
--- resources/views/permissions/edit.blade.php
+++ resources/views/permissions/edit.blade.php
@@ -1,24 +1,28 @@
 <x-master-layout>
     <div class="header">
-        <h1 class="header-title">
-            WebTech APP
-        </h1>
-        <p class="header-subtitle">{{ __('Edit permission') }}</p>
+        <h1 class="header-title">{{ __('Permissions') }}</h1>
     </div>
     <div class="row">
-        <div class="container col-md-6 m-2">
-            <h1>Editar Permiso</h1>
-            <form action="{{ route('permissions.update', $permission->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" id="name" name="name" value="{{ $permission->name }}"
-                        required>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h1>{{ __('Edit permission') }}</h1>
                 </div>
-                <button type="submit" class="btn btn-primary">Actualizar</button>
-                <a href="{{ route('permissions.index') }}" class="btn btn-secondary ms-2">Cancelar</a>
-            </form>
+                <div class="card-body">
+                    <form action="{{ route('permissions.update', $permission->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="name" class="form-label">{{ __('Permission name') }}</label>
+                            <input type="text" class="form-control" id="name" name="name"
+                                value="{{ $permission->name }}" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">{{ __('Update') }}</button>
+                        <a href="{{ route('permissions.index') }}"
+                            class="btn btn-secondary ms-2">{{ __('Cancel') }}</a>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </x-master-layout>
